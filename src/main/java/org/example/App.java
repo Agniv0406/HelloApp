b787ef2c3167230a96475f0bcbb328acc6ba1193
Agniv0406 <agniv0406@gmail.com>
2026-03-18 11:04:13 +0530

    feature/UC2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,10 @@
-package org.example;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        if(args.length > 0){
+            String name = args[0];
+            System.out.println("Hello " + name + " !");
+        } else {
+            System.out.println("Please provide a name as argument.");
+        }
     }
 }
