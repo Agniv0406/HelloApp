f51dbbcb22ed563c5a365dedd9bc0b3046f17954
Agniv0406 <agniv0406@gmail.com>
2026-03-18 11:26:49 +0530

    Feature/UC3

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,10 +4,12 @@ package org.example;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("enter the ");
+        String name = args[0];
+        System.out.println( "Hello " + name + " !" );
     }
 }
