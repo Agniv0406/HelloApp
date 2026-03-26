7f27b693ed691fe1c67675e179b33706cd36de6e
Agniv0406 <agniv0406@gmail.com>
2026-03-26 10:25:28 +0530

    Feature/UC5

--- src/main/java/org/example/HelloApp.java
+++ src/main/java/org/example/HelloApp.java
@@ -2,10 +2,18 @@ package org.example;
 
 public class HelloApp {
     public static void main(String[] args) {
+        // Step 1: Check if arguments are provided
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            System.out.println("Hello, " + String.join(", ", args) + "!");
+            StringBuilder names = new StringBuilder();
+            for (String name : args) {
+                if (names.length() > 0) {
+                    names.append(", ");
+                }
+                names.append(name);
+            }
+            System.out.println("Hello, " + names.toString() + "!");
         }
     }
 }
