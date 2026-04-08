2a2e5bdcc6cacb86e28c8f97ec9ecd76b2210ad6
Agniv0406 <agniv0406@gmail.com>
2026-04-08 22:27:59 +0530

    feature/UC7

--- src/main/java/org/example/HelloApp.java
+++ src/main/java/org/example/HelloApp.java
@@ -2,18 +2,14 @@ package org.example;
 
 public class HelloApp {
     public static void main(String[] args) {
+
+        // Check if arguments are provided
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            StringBuilder nameBuilder = new StringBuilder();
-
-            for (String name : args) {
-                nameBuilder.append(name).append(", ");
-            }
-            if (nameBuilder.length() > 0) {
-                String finalNames = nameBuilder.substring(0, nameBuilder.length() - 2);
-                System.out.println("Hello, " + finalNames + "!");
-            }
+            // Join names using comma and space
+            String names = String.join(", ", args);
+            System.out.println("Hello, " + names + "!");
         }
     }
 }
